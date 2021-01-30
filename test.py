f6950ce39bf73853ddde11a6e97945ccd889418e
Hamad Javed <hamadjaved69@gmail.com>
2021-01-30 20:00:21 +0000

    First

---
+++ test.py
@@ -0,0 +1,9 @@
+print("Hello World")
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/Desktop/picture.jpg')
+camera.stop_preview()
---
+++ test_stepper.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+test = "Hamad"
+
+if test == "Hamad":
+	sleep(2.5)
+	print("abc")
+else:
+	print("Nope")
